16340b28732818d0424f1406d6cb935d1260aa6f
profeseur01 <awaloumassaoudou@gmail.com>
2025-06-17 01:01:21 +0100

    Insertion de la fonction qui permet le la selection des ping

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 // Declaration des variables
 const body = document.querySelector("body")
-const ping_right = document.getElementById("ping-right")
 const ping_left = document.getElementById("ping-left")
+const ping_right = document.getElementById("ping-right")
 
 // Fonction qui permet de changer les ping en prssant sur les deux fleche gauche drote de clavier
 const changePingFunction = (e) => {
 console.log(e);
 if (e.key == "ArrowLeft"){
 console.log("Fleche Gauche appuyer");
-
+ping_right.classList.remove("pingSelected")
+ping_left.classList.add("pingSelected")
 
 }
 else if(e.key == "ArrowRight"){
 console.log("Fleche Droite appuyer");
+ping_left.classList.remove("pingSelected")
+ping_right.classList.add("pingSelected")
 }
 
 }
